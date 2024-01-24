bb710350cb557758cac26bb2d1d1f25b5a58cc15
heri <heri_espino@outlook.com>
2024-01-23 18:02:57 -0600

    No

--- 005_Factorial_Profe.pl
+++ 005_Factorial_Profe.pl
@@ -1,7 +1,7 @@
-factorial(0, 1):- write('factorial of 1 is 1'), nl.
+factorial(0, 1):- write('factorial of 0 is 1'), nl.
 
 factorial(N, Result):-
     M is N - 1,
     factorial(M, SubResult),
     Result is N * SubResult,
-	write('factorial of '), write(Result), write(' is '), write(SubResult), nl
+	write('factorial of '), write(N), write(' is '), write(Result), nl.
